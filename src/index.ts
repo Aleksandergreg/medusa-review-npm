4069d9d1320f73f84859f14bded102f07662dbdc
Aleksandergreg <alek3691@stud.kea.dk>
2025-08-25 13:33:54 +0200

    Dependencies clean up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { ProductReview } from './models/product-review.js';
 import ProductReviewService from './services/product-review.js';
+import { ProductReviewCreate1755957549768 } from "./migrations/1755957549768-ProductReviewCreate.js";
 
-export {
-  ProductReview,
-  ProductReviewService
+export default {
+  models: [ProductReview],
+  services: [ProductReviewService],
+  migrations: [ProductReviewCreate1755957549768],
 };
