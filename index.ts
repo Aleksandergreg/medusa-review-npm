a3074d733a93e4a8685ad68213fdc197a062acbe
Aleksandergreg <alek3691@stud.kea.dk>
2025-08-25 09:04:32 +0200

    Added an index.ts so it's actually possible to run the migration :D

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { ProductReview } from './src/models/product-review';
+import ProductReviewService from './src/services/product-review';
+
+export {
+  ProductReview,
+  ProductReviewService
+};
